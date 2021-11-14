00a0d283e492a38dca1ba7c15fdeb5bb6ea9ca8f
André Barata <andre.quina.barata@gmai.com>
2021-11-14 21:33:56 +0000

    Starting daemon when not present

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 const axios = require('axios');
+const waitPort = require('wait-port');
 const { Command } = require('commander');
+const { Monitor } = require('forever-monitor');
 
 const program = new Command();
+
+function callOnApi(callUrl) {
+  return axios.get(`http://localhost:8078`)
+    .catch(() => {
+      new Monitor('./chromecast-server.js', {
+        max: 3,
+        silent: true,
+        args: []
+      }).start();
+
+      return waitPort({ host: 'localhost', port: 8078});
+    })
+    .then(() => axios.get(callUrl));
+}
 
 program.command('play')
   .argument('<mediaUrl>')
   .action((mediaUrl) => {
-    axios.get(`http://localhost:8078/play/${mediaUrl}`)
+    callOnApi(`http://localhost:8078/play/${mediaUrl}`)
   });
 
 program.command('pause')
   .action((mediaUrl) => {
-    axios.get(`http://localhost:8078/pause`)
+    callOnApi(`http://localhost:8078/pause`)
   });
 
 
 program.command('resume')
   .action((mediaUrl) => {
-    axios.get(`http://localhost:8078/resume`)
+    callOnApi(`http://localhost:8078/resume`)
   });
 
 program.command('stop')
   .action((mediaUrl) => {
-    axios.get(`http://localhost:8078/stop`)
+    callOnApi(`http://localhost:8078/stop`)
   });
 
 program.parse(process.argv);
